3297f6c13bdb774c8cdd010ecd62f28df54c0905
Srinivasa Rao <srinivasa.rao@nds.ox.ac.uk>
2022-01-18 23:29:01 +0000

    fix slides, add problem sets to code

--- session_1/3_cleaning_and_exploring_data.R
+++ session_1/3_cleaning_and_exploring_data.R
@@ -40,3 +40,13 @@ x > 4 | x > 6
 
 # Filtering with more than one criterion
 migmorph[which_rows_mig1 | which_rows_elongatedness1.5, ]
+
+#### Problem set ####
+# Use the migmorph data.frame for these questions
+
+# Q1: Knockdown of which gene results in the highest migration?
+# # hint: this can be accomplished with the max() function
+
+# Q2: Make a new data.frame that has only rows with 0.9 > migration > 0.4 AND elongatedness > 1.4
+# Q2.1: What is the correlation between migration and elongatedness in this new dataset?
+# Q2.2: Draw an XY plot showing the relationship between these two variables in this new dataset
