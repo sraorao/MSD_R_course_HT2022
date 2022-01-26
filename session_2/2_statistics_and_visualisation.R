8d62e86992f6591c1819dfff5c84111839aecdd8
Srinivasa Rao <srinivasa.rao@nds.ox.ac.uk>
2022-01-26 09:21:43 +0000

    update plotting

--- session_2/2_statistics_and_visualisation.R
+++ session_2/2_statistics_and_visualisation.R
@@ -5,8 +5,7 @@
 # We will load and subset migration and morphology data as in session 1
 
 # Load migration data
-# We are only interested in columns 8, 9, 10, 13
-# We are also NOT interested in rows 1, 2
+# We are only interested in columns 8, 10, 13
 migration_data = read.csv("session_1/data/AID_1159618_datatable_all.csv")
 migration_data = migration_data[c(-1, -2), c(8, 10, 13)]
 colnames(migration_data) = c("reagent_id", "gene_symbol", "migration")
@@ -16,7 +15,6 @@ head(migration_data)
 # Load morphology data
 # We are only interested in columns 8, 10, 19, 59
 # NOTE: we are reading column 59 in addition this time
-# We are also NOT interested in rows 1, 2
 morphology_data = read.csv("session_1/data/AID_1159617_datatable_all.csv")
 morphology_data = morphology_data[c(-1, -2), c(8, 10, 19, 59)]
 colnames(morphology_data) = c("reagent_id", "gene_symbol", "elongatedness", "cluster")
@@ -33,8 +31,6 @@ str(migmorph)
 migmorph$migration = as.numeric(migmorph$migration)
 migmorph$elongatedness = as.numeric(migmorph$elongatedness)
 
-
-
 #### Statistics ####
 summary(migmorph)
 mean(migmorph$migration)
@@ -43,7 +39,6 @@ median(migmorph$migration)
 # T test
 # Filter the data based on the cluster column and perform a t test 
 # between clusters 1 and 6
-# Do the above using split() (needs List?)
 migmorph_cl1 = migmorph[migmorph$cluster == "1", ]
 migmorph_cl6 = migmorph[migmorph$cluster == "6", ]
 t.test(migmorph_cl1$elongatedness, migmorph_cl6$elongatedness)
@@ -52,29 +47,38 @@ t.test(migmorph_cl1$elongatedness, migmorph_cl6$elongatedness)
 # hint: look up the help for t.test()
 
 
+# Wilcoxon test
 wilcox.test(migmorph_cl1$elongatedness, migmorph_cl6$elongatedness)
 
 # Calculate Pearson's correlation coefficient
-
 cor.test(migmorph$migration, migmorph$elongatedness)
+
 # Q: Calculate Spearman correlation coefficient
 # # hint: Look up the help pages for cor.test() function
 
 
 #### Basic visualisation ####
 
+# Histogram and density plots
 hist(migmorph$migration) 
 hist(migmorph$migration, breaks = 10)
 
+density(migmorph$migration)
 plot(density(migmorph$migration))
 
+# X-Y plot
 plot(migmorph$migration, migmorph$elongatedness)
 
 # add colours
 plot(migmorph$migration, migmorph$elongatedness, col=migmorph$cluster)
 
-# Add a main title
+# Q: Add a main title
 
-# Change x and y labels
+# Q: Change x and y labels
 
+# barplot
+barplot(migmorph$migration)
 
+# boxplot
+boxplot(migmorph$migration, migmorph$elongatedness)
+boxplot(elongatedness ~ cluster, data = migmorph)
